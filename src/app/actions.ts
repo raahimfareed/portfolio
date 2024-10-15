cc1772bfb835799cd2cff54029c671081e9ddcc8
Raahim Fareed <raahimfareed@proton.me>
2024-10-15 19:55:30 +0500

    created encoding and decoding functions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,5 +1,8 @@
 "use server"
 import { cookies } from "next/headers";
+import { SignJWT, jwtVerify } from "jose";
+
+const key = new TextEncoder().encode(process.env.AUTH_SECRET)
 
 export async function setTheme(theme: string) {
   cookies().set('theme', theme);
@@ -8,4 +11,21 @@ export async function setTheme(theme: string) {
 export async function getTheme() {
   const theme = cookies().get('theme');
   return theme?.value ?? "";
+}
+
+// eslint-disable-next-line
+export async function encodeAndSign(payload: any) {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('7 days')
+    .sign(key)
+}
+
+export async function decodeAndVerifySignature(input: string) {
+  const { payload } = await jwtVerify(input, key, {
+    algorithms: ['HS256'],
+  });
+
+  return payload;
 }
