4f33025e30ae7770d295881288cceac5eb780a5c
Raahim Fareed <raahimfareed@proton.me>
2024-10-15 19:55:03 +0500

    created middleware admin

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { NextRequest } from "next/server";
+import { updateSession } from "./lib";
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: '/admin/:path*'
+}
