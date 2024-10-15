c2b17c0bdf1bde78d26c658c3724867118877f8e
Raahim Fareed <raahimfareed@proton.me>
2024-10-15 17:10:03 +0500

    removed login function from actions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,18 +1,11 @@
 "use server"
-
 import { cookies } from "next/headers";
 
 export async function setTheme(theme: string) {
   cookies().set('theme', theme);
 }
 
-
 export async function getTheme() {
   const theme = cookies().get('theme');
   return theme?.value ?? "";
-}
-
-export async function login(formData: FormData) {
-  // eslint-disable-next-line
-  const email = formData.get('email');
 }
