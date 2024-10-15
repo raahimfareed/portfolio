4a59cc3a3ea5f7a36c25e3c0ba6306cb81a737f5
Raahim Fareed <raahimfareed@proton.me>
2024-10-15 19:56:40 +0500

    created a dashboard page for admin

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste quia obcaecati reiciendis voluptatibus sed placeat minima delectus doloribus, temporibus quos? Obcaecati consequatur voluptates inventore doloremque enim ratione veniam, quibusdam illo?
+    </div>
+  )
+}
+
+export default AdminDashboard
