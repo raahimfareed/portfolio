4c7a7ad8c0f090f7a133bda318d9de10745e23b2
Raahim Fareed <raahimfareed@proton.me>
2025-04-12 05:44:40 +0500

    updated contact details and removed form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,11 +12,9 @@ import Image from "next/image";
 const Contact = () => {
   return (
     <MainLayout>
-      <section className='px-8 xl:px-16 flex flex-col lg:flex-row gap-8 xl:gap-16 lg:py-0 pb-24'>
-        <div className="pt-24 lg:py-48 h-full lg:sticky left-0 top-0 w-64 text-foreground flex flex-col gap-4">
-          <h1 className="text-4xl lg:text-2xl font-bold">
-            My Socials
-          </h1>
+      <section className='px-8 xl:px-16 flex flex-col lg:flex-row gap-8 xl:gap-16 lg:py-0 pb-24 min-h-screen items-center justify-center'>
+        <div className="flex flex-col gap-4 lg:py-48 overflow-y-auto overflow-x-hidden w-full lg:w-[64ch]">
+          <h1 className="text-4xl font-bold">Get In Contact</h1>
           <ContactSidebarLink href="https://linkedin.com/in/raahimfareed">
             <Image src={linkedin} alt="Linkedin" className="w-4 h-4" />
             @raahimfareed
@@ -25,43 +23,10 @@ const Contact = () => {
             <FaGithub className="w-4" />
             @raahimfareed
           </ContactSidebarLink>
-          <ContactSidebarLink href="https://instagram.com/raahimwho">
-            <Image src={instagram} alt="Instagram" className="w-4" />
-            @raahimwho
-          </ContactSidebarLink>
           <ContactSidebarLink href="mailto:raahimfareed@proton.me">
             <EnvelopeIcon className="w-4" />
             raahimfareed@proton.me
           </ContactSidebarLink>
-          <ContactSidebarLink href="mailto:raahimfareed@proton.me">
-            <BookOpenIcon className="w-4" />
-            Raahim&apos;s Blog
-          </ContactSidebarLink>
-        </div>
-        <div className="flex flex-col gap-4 lg:py-48 overflow-y-auto overflow-x-hidden w-full lg:w-[64ch]">
-          <h1 className="text-4xl font-bold">Get In Contact</h1>
-          <div className="flex flex-col gap-2">
-            <form className="rounded-xl p-8 bg-secondary text-secondary-foreground">
-              <p className="rounded-lg p-4 mb-3 text-yellow-900 border-2 border-yellow-500 bg-yellow-200">Contact page is not functional at the moment! It&apos;s under development, please use my socials to get in contact</p>
-              <div className="mb-3">
-                <Label>Name</Label>
-                <Input className="border-background bg-background text-foreground" required />
-              </div>
-              <div className="mb-3">
-                <Label>Email</Label>
-                <Input className="border-background bg-background text-foreground" required />
-              </div>
-              <div className="mb-3">
-                <Label>Subject</Label>
-                <Input className="border-background bg-background text-foreground" required />
-              </div>
-              <div className="mb-3">
-                <Label>Message</Label>
-                <Textarea className="border-background bg-background text-foreground" />
-              </div>
-              <button className="rounded-lg border-2 border-accent py-1 px-4 hover:bg-accent hover:text-accent-foreground transition-colors">Send Message</button>
-            </form>
-          </div>
         </div>
       </section>
     </MainLayout>
