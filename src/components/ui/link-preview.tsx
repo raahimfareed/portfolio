19151c9e762b52a3d73a34dbc605a5995086326e
Raahim Fareed <raahimfareed@proton.me>
2025-04-12 05:43:53 +0500

    made it so that the links open in a new tab `target='_blank'`

--- src/components/ui/link-preview.tsx
+++ src/components/ui/link-preview.tsx
@@ -101,6 +101,7 @@ export const LinkPreview = ({
                 <HoverCardPrimitive.Trigger
                     onMouseMove={handleMouseMove}
                     className={cn("text-black dark:text-white", className)}
+                    target="_blank"
                     href={url}
                 >
                     {children}
@@ -134,6 +135,7 @@ export const LinkPreview = ({
                             >
                                 <Link
                                     href={url}
+                                    target="_blank"
                                     className="block p-1 bg-white border-2 border-transparent shadow rounded-xl hover:border-neutral-200 dark:hover:border-neutral-800"
                                     style={{ fontSize: 0 }}
                                 >
