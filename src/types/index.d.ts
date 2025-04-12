53422d9bb87cbe67d740377ee9e38a590a21ff77
Raahim Fareed <raahimfareed@proton.me>
2025-04-12 05:42:10 +0500

    added tech pill types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,6 @@
+import { StaticImageData } from "next/image";
+import { ReactNode } from "react";
+
 export interface Theme {
     [key: string]: {
         [key: string]: string;
@@ -5,10 +8,51 @@ export interface Theme {
 }
 
 export interface Project {
-    id?: number;
-    image?: string;
+    image?: StaticImageData;
     name: string;
-    description: string;
+    description: string | ReactNode;
     url?: string;
-    techStack?: string[];
+    techStack?: TechType[];
 }
+
+export type PreferredTech =
+  | "postman"
+  | "express"
+  | "pycord"
+  | "django"
+  | "flask"
+  | "prisma"
+  | "vercel"
+  | "nextjs"
+  | "csharp"
+  | "alpinejs"
+  | "livewire"
+  | "jetpack-compose"
+  | "jquery"
+  | "bash"
+  | "jetbrains"
+  | "kotlin"
+  | "neovim"
+  | "vscode"
+  | "laravel"
+  | "react"
+  | "archlinux"
+  | "php"
+  | "html"
+  | "css"
+  | "sass"
+  | "javascript"
+  | "typescript"
+  | "python"
+  | "unity"
+  | "tailwindcss"
+  | "mysql"
+  | "postgresql"
+  | "arduino"
+  | "docker"
+  | "figma"
+  | "git"
+  | "jira"
+  | "postcss";
+
+export type TechType = PreferredTech | (string & {});
