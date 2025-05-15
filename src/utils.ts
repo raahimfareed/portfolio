aab15239218235269a3c0f46e003362680c1e358
Raahim Fareed <raahimfareed@proton.me>
2025-05-15 19:57:52 +0500

    removed prisma

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
-export const prisma = new PrismaClient();
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
