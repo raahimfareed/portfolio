339ab1a4eb334addeb1d8f8d2ef5831bb883383a
Raahim F. <raahim@neoflux.pk>
2026-08-26 19:41:23 +0500

    chore(fonts): changed poppins to alata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Poppins } from "next/font/google";
+import { Poppins, Alata } from "next/font/google";
 import "./globals.css";
 import { cookies } from "next/headers";
 import type { Metadata } from "next";
@@ -6,6 +6,11 @@ import type { Metadata } from "next";
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+const alata = Alata({
+  subsets: ['latin'],
+  weight: [ "400" ]
 });
 
 export const metadata: Metadata = {
@@ -22,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme={!!theme ? theme.value : "Mono"} data-style={!!style ? style.value : "Minimal"}>
       <body
-        className={`${poppins.className} antialiased bg-background text-foreground overflow-x-hidden`}
+        className={`${alata.className} antialiased bg-background text-foreground overflow-x-hidden`}
       >
         {children}
       </body>
